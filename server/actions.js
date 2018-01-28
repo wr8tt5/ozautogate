dee6a25c1dea623b067f3e6c856613f007afe1f5
Wayne Ellison <wr8tt5@gmail.com>
2018-01-28 11:54:17 +1000

    Use environment variables for email credentials.

--- server/actions.js
+++ server/actions.js
@@ -2,32 +2,42 @@ var email = require('emailjs/email');
 
 exports.sendquote = function(req, res) {
 
-  if (false) {
-  var server = email.server.connect({
-         user: 'wr8tt5'
-    ,password: 'blahblah'
-        ,host: 'smtp.mail.yahoo.com.au'
-         ,ssl: true
-  });
+  const quoteEmailUser = process.env.QUOTE_EMAIL_USER;
+  const quoteEmailPwd = process.env.QUOTE_EMAIL_PWD;
 
-  server.send({
-    text: 'This is a test',
-    from: 'wr8tt5@yahoo.com.au',
-    to:   'wr8tt5@yahoo.com.au',
-    subject: 'This is a test'},
-    function(err, message) {
-      if (err) {
-        console.log(err);
-        res.json({success: false, msg: 'failed to send'});
-      }
-      else {
-        res.json({success: true, msg: 'sent'});
-      }
-    }
-  );
+  if (quoteEmailUser == '' || quoteEmailPwd == '') {
+    console.log('No email credentials are present');
   }
+  else {
+    var server = email.server.connect({
+           user: quoteEmailUser
+      ,password: quoteEmailPwd
+          ,host: 'smtp.mail.yahoo.com.au'
+           ,ssl: true
+    });
 
-  console.log('quote sent');
-  console.log(req.body);
-  res.json({success: true, msg: 'quote sent'});
+    var msg = `
+Name:     ${req.body.name}
+Email:    ${req.body.email}
+Country:  ${req.body.country}
+State:    ${req.body.state}
+Comments: ${req.body.comments}
+`;
+
+    server.send({
+      text: msg,
+      from: `${quoteEmailUser}@yahoo.com.au`,
+      to:   `${quoteEmailUser}@yahoo.com.au`,
+      subject: 'Autogate quote request'},
+      function(err, message) {
+        if (err) {
+          console.log(err);
+          res.json({success: false, msg: 'failed to send'});
+        }
+        else {
+          res.json({success: true, msg: 'sent'});
+        }
+      }
+    );
+  }
 }
